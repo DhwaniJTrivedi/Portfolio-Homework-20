e1d70c04c18d9c2269002185b585e0e00ab8b742
DhwaniJTrivedi <127465429+DhwaniJTrivedi@users.noreply.github.com>
2023-08-08 16:02:07 -0400

    scroll changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-
+$(document).ready(function() {
+  $('.animate__fadeInUp').waypoint({
+    handler: function(direction) {
+      if (direction === 'down') {
+        $(this.element).addClass('animate__animated animate__fadeInUp');
+      }
+    },
+    offset: '90%', // Adjust this value as needed
+  });
+});
 $(document).ready(function() {
   $('.navbar-toggler').on('click', function() {
     $('#navbarSupportedContent').collapse('toggle');
