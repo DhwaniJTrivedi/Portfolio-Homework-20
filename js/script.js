f9a00b2b0c6e5d30000ba791c5b291748bb76d1f
DhwaniJTrivedi <127465429+DhwaniJTrivedi@users.noreply.github.com>
2023-08-09 17:39:02 -0400

    progress while scrolling added

--- js/script.js
+++ js/script.js
@@ -22,10 +22,17 @@ $(document).ready(function() {
   });
 });
 
+// $(document).on("scroll", function(){
+//   var pixels = $(document).scrollTop();
+//   var pageHeight = $(document).height() - $(window).height();
+//   var progress = 100 * pixels / pageHeight;
+  
+//   $("div.progress").css("width", progress + "%");
+// });
 $(document).on("scroll", function(){
   var pixels = $(document).scrollTop();
   var pageHeight = $(document).height() - $(window).height();
   var progress = 100 * pixels / pageHeight;
   
   $("div.progress").css("width", progress + "%");
-})
+});
